35c1377a2864cbc7246fed028acb7ce4834edac1
MaheshKotambe <technomk99@gmail.com>
2026-01-14 14:16:17 +0530

    next commit

---
+++ arrays/arrays.js
@@ -0,0 +1,11 @@
+var a= [1,2,3,4,5]
+b=a
+//b=[...a] // spread - a will change but b will not
+
+a.pop()
+
+console.log(a)
+console.log(b)
+
+let c='c'
+console.log(typeof(c))
---
+++ basics/basics.js
@@ -0,0 +1,7 @@
+// var a = "vishal"
+// console.log(a)
+a=10
+b=a
+a--
+console.log(a)
+console.log(b)
